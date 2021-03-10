2f94f4efafd5141dd5e479aff4931d52489ed687
edansneh <edan.sneh@gmail.com>
2021-03-09 21:30:30 -0800

    Fixed generative model and added discriminative model

--- models/discriminative.py
+++ models/discriminative.py
@@ -1,17 +1,54 @@
 # FIX THIS: rename however you want
+from generative import Down
+import torch
+import torch.nn as nn
 
 class Discriminative(nn.Module):
-    def __init__(self, image_size):
+    def __init__(self, image_size, rgb = True):
         super(Generative, self).__init__()
-        if (image_size % 4 != 0):
-            raise Exception("Image must be a factor of 4")
+        if (image_size % 8 != 0):
+            raise Exception("Image must be a factor of 8")
         # in_channels, num filters, kernal_size
+        filters1 = 32   #32
+        filters2 = 64   #64
+        filters3 = 128   #128
+        # filters4 = 256   #256
+        padding = 1 # to ensure convolutions dont change size
+        # FIX THIS: delete n
+        n = image_size/4 #each image is 1 feature at this point dependent on image_size
+
+        # size x / 2
+        if (rgb) {
+            self.d1 = Down(3, filters1) # rgb channels 
+        } else {
+            self.d1 = Down(1, filters1) # rgb channels
+        }
+
+        # size x / 4
+        self.d2 = Down(filters1, filters2)
+
+        # size x / 8
+        self.d3 = Down(filters2, filters3)
+
+        hidden = 100
+        self.L = nn.Linear(filters3*filters3, hidden)
+        self.L2 = nn.Linear(hidden, 1)
+
+
 
     def forward(self, x):
-        x = torch.GlobalAveragePooling2d(1,1)
-        x = nn.Linear(3, 1)
-        return torch.sigmoid(x)
+        # size x / 2
+        x1 = self.d1(x)
+        # size x / 4
+        x2 = self.d2(x1)
+        # size x / 8
+        x3 = self.d3(x2)
 
+        y = x3.view(y.shape[0], y.shape[1], -1) # put in linear columns
+        y = self.L(y)
+        y = self.L2(y)
+        return y
 
+        
 
         
--- models/generative.py
+++ models/generative.py
@@ -6,7 +6,7 @@ class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
     def __init__(self, in_channels, out_channels):
-        super().__init__()
+        super(DoubleConv, self).__init__()
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -19,132 +19,101 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         return self.double_conv(x)
 
-
+# changed to double conv then maxpool
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
 
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
             DoubleConv(in_channels, out_channels)
+            nn.MaxPool2d(2),
         )
 
     def forward(self, x):
         return self.maxpool_conv(x)
-        
+
 class Up(nn.Module):
     """Upscaling then double conv"""
 
     def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
+        super(Up, self).__init__()
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
-
+            self.up = nn.Upsample(scale_factor=2) # nearest neighbor instead
         self.conv = DoubleConv(in_channels, out_channels)
 
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
+    def forward(self, x, h):
+        if (len(x.shape) != 4):
+            raise Exception("Input tensor is incorrect shape")
+        if (x.shape[2] != h.shape[2] or x.shape[3] != h.shape[3]):
+            raise Exception("shapes do not match for x and h!!!")
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
+        x = self.up(x)
+        # input is CHW
+        # diffY = x2.size()[2] - x1.size()[2]
+        # diffX = x2.size()[3] - x1.size()[3]
+        # x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        # diffY // 2, diffY - diffY // 2])
         # if you have padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
+        if h is not None:
+            x = torch.cat([x, h], dim=1) # attach on channel
         return self.conv(x)
 
-
-
-class DownSampleBlock(nn.Module):
-     def __init__(self, in_channels, out_channels):
-        super(DownSampleBlock, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Conv2d(in, out_channels),
-            nn.Conv2d(out, out),
-            nn.ReLU,
-            nn.BatchNorm(out),
-            nn.MaxPool2d(2)
-        )
-
-    def forward(self, x):
-        return self.layers(x)
-
-class UpSampleBlock(nn.Module):
-    
-    def forward(self, x, h):
-        self.layers(x) + h
-
-
 class Generative(nn.Module):
-    def __init__(self, image_size):
+    def __init__(self, image_size, rgb = True):
         super(Generative, self).__init__()
-        if (image_size % 4 != 0):
-            raise Exception("Image must be a factor of 4")
+        if (image_size % 8 != 0):
+            raise Exception("Image must be a factor of 8")
         # in_channels, num filters, kernal_size
-        filters1 = 20   #32
-        filters2 = 20   #64
-        filters3 = 20   #128
-        filters4 = 20   #256
-        filters5 = 20   #128
-        filters6 = 20   #64
-        filters7 = 10
+        filters1 = 32   #32
+        filters2 = 64   #64
+        filters3 = 128   #128
+        # filters4 = 256   #256
         padding = 1 # to ensure convolutions dont change size
         # FIX THIS: delete n
         n = image_size/4 #each image is 1 feature at this point dependent on image_size
-        self.c1 = nn.Conv2d(3, filters1, 3, padding=padding) #convolution for color image
-        self.c2 = nn.Conv2d(filters1, filters2, 3, padding=padding)
-        self.m1 = nn.MaxPool2d(2) # downsample by 2
-        self.c3 = nn.Conv2d(filters2, filters3, 3, padding = padding)
-        self.c4 = nn.Conv2d(filters3, filters4, 3, padding = padding)
-        self.m2 = nn.MaxPool2d(2) # downsample by 2
-        self.c5 = nn.Conv2d(filters4, filters5, 3, padding=padding)
-        self.c6 = nn.Conv2d(filters5, filters6, 3, padding=padding)
-        self.m3 = nn.MaxPool2d(2) # downsample by 2
-        self.c7 = nn.Conv2d(filters6, filters7, 1) # 1d conv to reduce filter size
 
-        # self.u1 = nn.MaxUnpool2d(n) # suggestion: Bilinear upsampling
-        self.d1 = nn.ConvTranspose2d(filters7, filters5, 3, padding=padding)
-        self.d2 = nn.ConvTranspose2d(filters5, filters4, 3, padding=padding)
-        # self.u2 = nn.MaxUnpool2d(2)
-        self.d3 = nn.ConvTranspose2d(filters4, filters3, 3, padding=padding)
-        self.d4 = nn.ConvTranspose2d(filters3, filters2, 3, padding=padding)
-        # self.u3 = nn.MaxUnpool2d(2)
-        self.d5 = nn.ConvTranspose2d(filters2, filters1, 3, padding=padding)
-        self.d6 = nn.ConvTranspose2d(filters1, 1, 3, padding=padding)
+        # size x / 2
+        if (rgb) {
+            self.d1 = Down(3, filters1) # rgb channels 
+        } else {
+            self.d1 = Down(1, filters1) # rgb channels
+        }
+
+        # size x / 4
+        self.d2 = Down(filters1, filters2)
+
+        # size x / 8
+        self.d3 = Down(filters2, filters3)
+        # self.onedim = nn.Conv2d(filters3, filters4, 1) # 1d conv to reduce filter size
+        bilinear = True
+        self.u1 = Up(filters3 + filters2, filters2, bilinear=bilinear)
+        self.u2 = Up(filters2 + filters1, filters1, bilinear=bilinear)
+        if (rgb) {
+            self.u3 = Up(filters1, 3, bilinear=bilinear) # rgb output
+        } else {
+            self.u3 = Up(filters1, 1, bilinear=bilinear) # grayscale output
+        }
 
     def forward(self, x):
-        x1 = self.c1(x)
-        x1 = self.c2(x1)
-        x2 = self.m1(x1)
-        x2 = self.c3(x2)
-        x2 = self.c4(x2)
-        x3 = self.m2(x2)
-        x3 = self.c5(x3)
-        x3 = self.c6(x3)
+        # size x / 2
+        x1 = self.d1(x)
+        # size x / 4
+        x2 = self.d2(x1)
+        # size x / 8
+        x3 = self.d3(x2)
 
-        #x4 = 1xFilters
-        x4 = self.m3(x3)
-        x4 = self.c7(x4)
+        # size x / 4
+        y = self.u1(x3, x2) # upsample x3 and and concat with x2
+        # size x / 2
+        y = self.u2(y, x1) # upsample y and concat with x1
+        # size x
+        y = self.u3(y, None)
+        return y # image should be same size as x
 
-        y = self.u1(x4) + x3
-        y = self.d1(y)
-        y = self.d2(y)
-        y = self.u2(y) + x2
-        y = self.d3(y) 
-        y = self.d4(y) 
-        y = self.u3(y) + x1
-        y = self.d5(y)
-        y = self.d6(y)
-        return y
-
-
-
-        
